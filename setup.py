209f9a3efb46d437ed0f97574732d389b1efac6e
Kai Chang <kaijchang@gmail.com>
2018-10-13 18:11:22 -0700

    packaging with setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pictory',
+    version='0.1.0',
+    url='https://github.com/ramonsaraiva/pictory',
+    author='Ramon Saraiva',
+    author_email='ramonsaraiva@gmail.com',
+    description='simple tool to organize my pictures in lickable directories',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().split('\n')[:-1],
+    entry_points={
+        'console_scripts': ['pictory=pictory.pictory:main'],
+    }
+)
