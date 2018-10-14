2e03c33be87edfbe95dfe45580f333ab15676dfd
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-10-14 19:48:33 -0300

    Add long description to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
 
 setup(
     name='pictory',
@@ -7,6 +16,8 @@ setup(
     author='Ramon Saraiva',
     author_email='ramonsaraiva@gmail.com',
     description='simple tool to organize pictures and videos in lickable directories',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['pictory=pictory.pictory:main'],
